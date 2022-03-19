0a8b54942920ab18bd9603171594f01d0ba2ec93
Amit Sharma <iamamitshrma@gmail.com>
2022-03-19 14:45:44 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b67e8e4d-7779-4892-925c-2b83a3cc74fc';
+export const MODAL_CLOSE_SEC = 2.5;
